a7739d123334f9023c547893e45b525c0e9d0c07
Jdollx <jenniferdolldev@gmail.com>
2024-08-13 17:53:27 -0400

    delete button works

--- client/src/components/ListModels.js
+++ client/src/components/ListModels.js
@@ -4,6 +4,19 @@ const ListModels = () => {
     // initializes models as an empty array
     const [models, setModels] = useState([]);
 
+    // delete function by id called to Delete button below
+    const deleteModel = async(id) => {
+        try {
+            const deleteModel = await fetch(`http://localhost:3000/models/${id}`,{
+                method: "DELETE"
+            });
+            // filter function creates a new array with all models whose ID is not equal to the deleted model's ID => shows remaining models
+            setModels(models.filter(model => model.model_id != id ))
+        } catch (error) {
+            console.error(error.message); 
+        }
+    };
+
     // fetches models from the API
     const getModels = async () => {
         try {
@@ -25,15 +38,19 @@ const ListModels = () => {
     return (
         <div className="flex flex-col justify-center items-center bg-white mt-16">
             <ul id="model-list" className="flex flex-wrap justify-center gap-10 w-full">
-                {models.map((model, index) => (
-                    <li key={index} className="flex flex-col p-4 border rounded shadow-md w-64">
+                {models.map((model, model_id) => (
+                    <li key={model_id} className="flex flex-col p-4 border rounded shadow-md w-64">
                         <img src={model.imageUrl} alt={model.model_name} className="w-full h-32 object-cover mb-4" />
                         <h3 className="text-left text-lg font-semibold mb-2">{model.model_name}</h3>
                         <p className="text-left">Quantity: {model.quantity}</p>
+                        <p className="text-left">Tags: {model.quantity}</p>
                         <div className="flex gap-2 mt-4">
                             <button className="bg-green-500 text-white px-4 py-2 rounded">Logs</button>
                             <button className="bg-blue-500 text-white px-4 py-2 rounded">Edit</button>
-                            <button className="bg-red-500 text-white px-4 py-2 rounded">Delete</button>
+                            <button className="bg-red-500 text-white px-4 py-2 rounded"
+                            onClick={() => deleteModel(model.model_id)}>
+                            Delete
+                            </button>
                         </div>
                     </li>
                 ))}
