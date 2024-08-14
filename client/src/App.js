65d4d5bc468f66498391200b7120078a0e6887a8
Jdollx <jenniferdolldev@gmail.com>
2024-08-13 21:06:20 -0400

    filter bars formatted

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,18 @@ import AddModels from './components/AddModels';
 import ListModels from './components/ListModels';
 import FilterBar from './components/FilterBar';
 
-
 function App() {
   return (
-<>
-  <div className="containter">
-    <AddModels />
-  </div>
-  <div className="container">
-    <FilterBar />
-    <ListModels />
-  </div>
-</>
-)};
+    <>
+      <div className="container mx-auto px-4">
+        <AddModels />
+      </div>
+      <div className="container mx-auto px-4">
+        <FilterBar />
+        <ListModels />
+      </div>
+    </>
+  );
+}
 
 export default App;
