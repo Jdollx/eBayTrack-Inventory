93e7dfc1e8b9ce33a489ef359a89b669f0f3056e
Jdollx <jenniferdolldev@gmail.com>
2024-08-16 10:50:31 -0400

    model image correctly formatted

--- client/src/components/AddModels.js
+++ client/src/components/AddModels.js
@@ -14,7 +14,6 @@ const AddModels = () => {
   const [sale_quantity, setSaleQuantity] = useState("");
   const [tags, setTags] = useState("");
 
-  // Function to add a new model
   const AddNewModels = async () => {
     try {
       const body = {
@@ -28,20 +27,19 @@ const AddModels = () => {
         sale_date,
         sale_price,
         sale_quantity,
-        tags
+        tags,
       };
 
-      // Make POST request to add input to server
       const response = await fetch("http://localhost:3000/models", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
       });
 
-        const newModel = await response.json();
-        console.log("Model saved:", newModel);
-        closeModal();
-        window.location = "/";
+      const newModel = await response.json();
+      console.log("Model saved:", newModel);
+      closeModal();
+      window.location = "/";
     } catch (error) {
       console.error("Error saving model:", error.message);
     }
@@ -67,16 +65,15 @@ const AddModels = () => {
 
   return (
     <>
-        {/* Fixed button in the top right corner */}
-        <div className="fixed top-4 right-4 z-50">
+      <div className="fixed top-4 right-4 z-50">
         <button
-            className="px-4 py-2 bg-gray-400 text-white rounded"
-            onClick={openModal}
+          className="px-4 py-2 bg-gray-400 text-white rounded"
+          onClick={openModal}
         >
-            Add New Model
+          Add New Model
         </button>
-        </div>
-
+      </div>
+  
       {isModalOpen && (
         <div
           id="crud-modal"
@@ -84,11 +81,11 @@ const AddModels = () => {
           aria-hidden="true"
           className="fixed inset-0 z-50 flex items-center justify-center w-full h-full overflow-x-hidden overflow-y-auto bg-gray-800 bg-opacity-50"
         >
-          <div className="relative w-full max-w-md p-4 max-h-full">
+          <div className="relative w-full max-w-3xl p-6 max-h-screen">
             <div className="relative bg-white rounded-lg shadow-md">
               <div className="flex items-center justify-between p-4 border-b border-gray-200 rounded-t">
                 <h3 className="text-lg font-semibold text-gray-900">
-                  Add New
+                  Add New Model
                 </h3>
                 <button
                   type="button"
@@ -107,25 +104,45 @@ const AddModels = () => {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                      d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7l-6 6"
                     />
                   </svg>
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
-
-            {/*MODEL NAME INPUT*/}
-            <form className="p-4" onSubmit={HandleSubmit}>
-                <div className="grid gap-4 mb-4 grid-cols-2">
-                    {/* Model Name Input */}
-                    <div className="col-span-2">
-                    <label
+  
+              {/* Add Model */}
+              <form className="p-6" onSubmit={HandleSubmit}>
+                <div className="flex gap-4 mb-4">
+                  <div className="flex flex-col items-center w-1/3">
+                    {model_image ? (
+                      <img
+                        src={model_image}
+                        alt="Model"
+                        className="object-cover w-full h-40 rounded-lg"
+                      />
+                    ) : (
+                      <div className="flex items-center justify-center w-full h-40 bg-gray-200 rounded-lg">
+                        <span className="text-gray-500">No photo available</span>
+                      </div>
+                    )}
+                    <input
+                      className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 mt-2"
+                      id="file_input"
+                      type="file"
+                      onChange={(e) => setModelImage(e.target.files[0])}
+                    />
+                  </div>
+  
+                  <div className="flex flex-col w-2/3">
+                    <div className="mb-4">
+                      <label
                         htmlFor="model_name"
                         className="block mb-2 text-sm font-medium text-gray-900"
-                    >
+                      >
                         Model Name:
-                    </label>
-                    <input
+                      </label>
+                      <input
                         type="text"
                         id="model_name"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
@@ -133,33 +150,30 @@ const AddModels = () => {
                         value={model_name}
                         onChange={(e) => setModel_Name(e.target.value)}
                         required
-                    />
+                      />
                     </div>
-
-                    {/* Quantity Input */}
-                    <div className="col-span-2">
-                    <label
+  
+                    <div>
+                      <label
                         htmlFor="quantity-input"
                         className="block mb-2 text-sm font-medium text-gray-900"
-                    >
+                      >
                         Quantity:
-                    </label>
-                    <input
+                      </label>
+                      <input
                         type="number"
                         id="quantity-input"
                         aria-describedby="helper-text-explanation"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                        placeholder="90210"
+                        placeholder="Enter quantity"
                         value={model_quantity}
                         onChange={(e) => setModel_Quantity(e.target.value)}
                         required
-                    />
+                      />
                     </div>
+                  </div>
                 </div>
-
-
-
-
+  
                 <button
                   type="submit"
                   className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
@@ -174,7 +188,7 @@ const AddModels = () => {
                       fillRule="evenodd"
                       d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
                       clipRule="evenodd"
-                    ></path>
+                    />
                   </svg>
                   Save changes
                 </button>
@@ -185,6 +199,6 @@ const AddModels = () => {
       )}
     </>
   );
-};
+};  
 
 export default AddModels;
